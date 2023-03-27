1e1401e64ee760e222e99c05969808084acfd551
MSreeN <khyathimnsree369@gmail.com>
2023-03-27 20:47:09 +0530

    added cart update functionality

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,15 +1,23 @@
+import { useContext } from "react";
 import Button from "../Components/UI/Button";
 import Modal from "../Components/UI/Modal";
+import CartContext from "../Store/cart-context";
 import classes from "./Cart.module.css";
 
 function Cart(props) {
+  const cartContext = useContext(CartContext);
+  console.log(cartContext.items);
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "b1", name: "sushi", amount: 2, price: 55 }].map((cartItem) => (
+      {cartContext.items.map((cartItem) => (
         <li>{cartItem.name}</li>
       ))}
     </ul>
   );
+
+  const totalAmount = cartContext.items.reduce((acc, cVal) =>{
+    return Math.floor(acc) + cVal.price
+  },0)
   console.log(cartItems);
 
   return (
@@ -17,7 +25,7 @@ function Cart(props) {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>55.66</span>
+        <span>{Math.floor(totalAmount)}</span>
       </div>
       <div className={classes.actions}>
         <Button
